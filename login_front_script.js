1e7fc4024845c766a5ce23e9a175c67962362ceb
unknown <vidub04@gmail.com>
2026-04-12 13:13:48 +0530

    separate css & java files

---
+++ login_front_script.js
@@ -0,0 +1,24 @@
+let isLogin = true;
+
+function toggleMode() {
+  isLogin = !isLogin;
+
+  document.getElementById("title").innerText = isLogin ? "Login" : "Create Account";
+  document.getElementById("submitBtn").innerText = isLogin ? "Login" : "Sign Up";
+  document.getElementById("extraFields").classList.toggle("hidden");
+
+  document.getElementById("toggleText").innerText = isLogin 
+    ? "Don't have an account?" 
+    : "Already have an account?";
+
+  document.getElementById("toggleBtn").innerText = isLogin ? "Sign Up" : "Login";
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const batch = document.getElementById("batch").value;
+
+  alert(`Welcome ${name} 🚀\nBatch: ${batch}`);
+}
+
